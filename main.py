9063bb4bbb0b328b305be98d26e723102a3b21ca
Yun-tellije <dbswjddms517@naver.com>
2024-04-05 10:48:41 +0900

    Add jinja2 template

--- main.py
+++ main.py
@@ -3,13 +3,12 @@
 # reload   -> 코드 변경 시 자동으로 다시 켜줌(저장 시)
 
 import uvicorn
-
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
-@app.get("/")
-async def welcome() -> dict:
-    return {
-        "message": "Hello FastAPI!"
-    }
+@app.get("/") # http://127.0.0.1:8000
+async def welcome(request: Request) -> dict:
+    return templates.TemplateResponse("index.html", {"request": request})
