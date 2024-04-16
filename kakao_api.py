648141cb6de09863cf6c351527ab28e4007a4666
Yun-tellije <dbswjddms517@naver.com>
2024-04-16 12:45:12 +0900

    Add kakao API

--- kakao_api.py
+++ kakao_api.py
@@ -4,6 +4,7 @@
 #  3. 인증 코드를 사용해서 토큰 발급
 #  4. 토큰을 사용해서 나에게 메세지 보내기
 import requests
+import json
 
 
 # 1. 카카오 OAUTH URL과 Redirect Key를 사용해서 인증 코드 요청
@@ -18,9 +19,25 @@ data = {
     "grant_type": "authorization_code",
     "client_id": "6e12e1aade94d00919a0b270704ec890",    # RESTAPI KEY
     "redirect_uri": "http://127.0.0.1:8000",
-    "code": "H1TBkZaZEjhRXIgQFxOI8uLKplx8BdL9osWpXxsxYj19xxpBNOloTIR8hj4KPXNOAAABjsDrBvGSBpCp5rpDbg"
+    "code": "FobheMWvqc2QiJT-Xq1yzR2hdN30K_TMv5To9W8xegkGSD7VhzNZN9invKUKKiVOAAABjuTkv-Kvm_uHqQwxKA"
 }
 
-response = requests.post(url, data=data)
-tokens = response.json()
-print(tokens)
+# response = requests.post(url, data=data)
+# tokens = response.json()
+# print(tokens)
+
+# 3. 토큰 이용해서 나에게 톡 보내기
+access_token = "zBYBrvREyqS6-IlINge7oaB79b5uLtvVhzEKPXKXAAABjuTmP9mIenTzhLqDRQ"
+
+msg_url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+headers = {
+    "Authorization": "Bearer " + access_token
+}
+msg_data = {
+    "template_object": json.dumps({
+        "object_type": "text",
+        "text": "카카오톡 테스트",
+        "link": {"mobile_web_url": "https://www.naver.com"}
+    })
+}
+response = requests.post(msg_url, headers=headers, data=msg_data)
