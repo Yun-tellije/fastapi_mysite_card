8da1149ef9ac634f3c5d33df6ee851c6456f29eb
Yun-tellije <dbswjddms517@naver.com>
2024-05-18 17:16:10 +0900

    Add scheduler

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from backend.routes import kakao
+from backend.services.service_kakao import KakaoService
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
@@ -17,3 +18,14 @@ app.include_router(kakao.router, prefix="/kakao")
 @app.get("/") # http://127.0.0.1:8000
 async def welcome(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    # 스케줄러
+    # - 카카오톡 Refresh token을 재발급 받는 기술
+    # - 매월 1일 동작
+    sched = BackgroundScheduler()
+    sched.add_job(KakaoService().refresh_access_token, "cron", day="1", hour="0", id="refresh_token")
+    sched.start()
+    
+    # 웹서버 동작!
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
